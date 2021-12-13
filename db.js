6cd264731e80bc0ece5271ba265fb67354c60b38
kevser-tr <kevser.tanrikulu@student.ehb.be>
2021-12-13 18:52:10 +0100

    database + express start

---
+++ db.js
@@ -0,0 +1,44 @@
+const {
+    MongoClient
+} = require("mongodb");
+
+                                                                                                                                
+const url = "mongodb+srv://admin:admin@cluster0.liznr.mongodb.net/webcourseproject?retryWrites=true&w=majority";
+const client = new MongoClient(url);
+
+// The database to use
+const dbName = "webcourseproject";
+
+async function run() {
+    try {
+        await client.connect();
+        console.log("Connected correctly to server");
+        const db = client.db(dbName);
+
+        // Use the collection "people"
+        const col = db.collection("teams");
+
+        // Construct a document                                                                                                                                                              
+        let teamsDocument = {
+            id: "61b3b3880e1dcb655d0298fe",
+            teamname: "booster",
+            damage: "5",
+            member: "Vi,Jinx,Ziggs,Amumu,Lux"
+        }
+
+        // Insert a single document, wait for promise so we can read it back
+        const p = await col.insertOne(teamsDocument);
+        // Find one document
+        const myDoc = await col.findOne();
+        // gets the firts document 
+        // Print to the console
+        console.log(myDoc);
+
+    } catch (err) {
+        console.log(err.stack);
+    } finally {
+        await client.close();
+    }
+}
+
+run().catch(console.dir);
--- index.js
+++ index.js
@@ -1,43 +1,12 @@
-const {
-    MongoClient
-} = require("mongodb");
+const express = require('express')
+const app = express()
+const port = 3000
 
-// Replace the following with your Atlas connection string                                                                                                                                        
-const url = "mongodb+srv://admin:admin@cluster0.liznr.mongodb.net/webcourseproject?retryWrites=true&w=majority";
-const client = new MongoClient(url);
+app.get('/', (req, res) => {
+    console.log('my root api');
+  res.send('Hello World! kev')
+})
 
-// The database to use
-const dbName = "webcourseproject";
-
-async function run() {
-    try {
-        await client.connect();
-        console.log("Connected correctly to server");
-        const db = client.db(dbName);
-
-        // Use the collection "people"
-        const col = db.collection("teams");
-
-        // Construct a document                                                                                                                                                              
-        let teamsDocument = {
-            id: "61b3b3880e1dcb655d0298fe",
-            teamname: "booster",
-            damage: "5",
-            member: "Vi,Jinx,Ziggs,Amumu,Lux"
-        }
-
-        // Insert a single document, wait for promise so we can read it back
-        const p = await col.insertOne(teamsDocument);
-        // Find one document
-        const myDoc = await col.findOne();
-        // Print to the console
-        console.log(myDoc);
-
-    } catch (err) {
-        console.log(err.stack);
-    } finally {
-        await client.close();
-    }
-}
-
-run().catch(console.dir);
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
