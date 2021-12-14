7f95c8a7fbd9bf2a383074ed4b708a68bcea7051
kevser-tr <kevser.tanrikulu@student.ehb.be>
2021-12-14 21:06:02 +0100

    express+db conncetion

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
 const port = 3000
 
+app.use(bodyParser.json())
+
+
 app.get('/', (req, res) => {
-    console.log('my root api');
+  
+  console.log('my root api');
   res.send('Hello World! kev')
 })
+
+app.post('/addTeams', (req, res) => {
+  console.log(req.body)
+  res.send('data recieved')
+})
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
