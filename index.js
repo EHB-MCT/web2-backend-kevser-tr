99ef8b410fa237dcc5471a6b1cd031fe922a3650
kevser-tr <kevser.tanrikulu@student.ehb.be>
2021-12-15 17:38:29 +0100

    geen comment

--- index.js
+++ index.js
@@ -12,39 +12,39 @@ const {
 const url = "mongodb+srv://admin:admin@cluster0.liznr.mongodb.net/webcourseproject?retryWrites=true&w=majority";
 const client = new MongoClient(url);
 
-// // The database to use
-// const dbName = "webcourseproject";
+// The database to use
+const dbName = "webcourseproject";
 
-// async function run() {
-//   try {
-//     await client.connect();
-//     console.log("Connected correctly to server");
-//     const db = client.db(dbName);
+async function run() {
+  try {
+    await client.connect();
+    console.log("Connected correctly to server");
+    const db = client.db(dbName);
 
-//     // Use the collection "people"
-//     const col = db.collection("teams");
+    // Use the collection "people"
+    const col = db.collection("teams");
 
-//     // Construct a document                                                                                                                                                              
-//     let teamDocument = {
-//       "id": "61b3b3880e1dcb655d0298fe",
-//       "teamname": "booster",
-//       "damage": "5",
-//       "member": "Vi,Jinx,Ziggs,Amumu,kevser"
-//     }
+    // Construct a document                                                                                                                                                              
+    let teamDocument = {
+      "id": "61b3b3880e1dcb655d0298fe",
+      "teamname": "booster",
+      "damage": "5",
+      "member": "Vi,Jinx,Ziggs,Amumu,kevser"
+    }
 
-//     // Insert a single document, wait for promise so we can read it back
-//     const p = await col.insertOne(teamDocument);
-//     // Find one document
-//     const myDoc = await col.findOne();
-//     // Print to the console
-//     console.log(myDoc);
+    // Insert a single document, wait for promise so we can read it back
+    const p = await col.insertOne(teamDocument);
+    // Find one document
+    const myDoc = await col.findOne();
+    // Print to the console
+    console.log(myDoc);
 
-//   } catch (err) {
-//     console.log(err.stack);
-//   } finally {
-//     await client.close();
-//   }
-// }
+  } catch (err) {
+    console.log(err.stack);
+  } finally {
+    await client.close();
+  }
+}
 
 run().catch(console.dir);
 
