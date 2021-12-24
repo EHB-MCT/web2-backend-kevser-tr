c17edf44498d7676c7c0dfbb407dbf14ab556d89
kevser-tr <kevser.tanrikulu@student.ehb.be>
2021-12-24 05:37:43 +0100

    fix post

--- api/routes/create-teams.js
+++ api/routes/create-teams.js
@@ -2,33 +2,28 @@ const express = require("express");
 const router = express.Router();
 const client = require("../db/db");
 const DB = client.db(process.env.DATABASE_NAME);
-
 router.post('/create', (req, res) => {
-    const newTeam = req.body
-    console.log(newTeam)
+
+    const {attack, magic, defense, members} = req.body
 
     let membersArray = []
     let teamMembers = {}
 
-    teamMembers.name = "Durum"
-    teamMembers.photo = "https://upload.wikimedia.org/wikipedia/commons/thumb/6/64/Rolled_kebab.jpg/440px-Rolled_kebab.jpg",
-    membersArray.push(teamMembers)
-
-    // teamMembers.name = "Köfte"
-    // teamMembers.photo = "https://leblogdistanbul.b-cdn.net/wp-content/uploads/2014/12/kofte.jpg",
-    // membersArray.push(teamMembers)
-
-    // teamMembers.name = "Sarma"
-    // teamMembers.photo = "https://www.grandturkishbazaar.com/wp-content/uploads/2020/03/yaprak-sarma-stuffed-grape-leaves-5-500x500.jpg",
-    // membersArray.push(teamMembers)
+    for (const oneMember of members) {
+        teamMembers.name = oneMember.name
+        teamMembers.photo = oneMember.photo
+        membersArray.push(teamMembers)
+    }
 
     let query = { 
-        name: "Cyril TEAM",
-        damage: 0,
-        magic: 0,
-        defense: 0,
+        // name: "Cyril TEAM",
+        damage: attack,
+        magic: magic,
+        defense: defense,
         members: membersArray
     };
+
+    console.log("QUEERY:", query)
 
     try {
         DB.collection("teams")
